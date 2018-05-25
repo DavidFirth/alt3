f3268718f0176f0d52878537617e8ed7b36f294f
DavidFirth <davidf@veryspeedy.net>
2018-05-25 08:27:19 +0100

    Changed cookie notice config to simplify.

--- docs/assets/gdpr-cookie-notice/config.js
+++ docs/assets/gdpr-cookie-notice/config.js
@@ -5,8 +5,8 @@ gdprCookieNotice({
   domain: '.alt-3.uk', //If you run the same cookie notice on all subdomains, define the main domain starting with a .
   implicit: false, //Deny cookies on scroll
   statement: '/cookies.html', //Link to your cookie statement page
-  performance: ['mathjax'], //Cookies in the performance category.
-  analytics: ['ga'], //Cookies in the analytics category.
-  marketing: ['lang'] //Cookies in the marketing category.
+//  performance: ['mathjax'], //Cookies in the performance category.
+  analytics: ['statcounter'], //Cookies in the analytics category.
+//  marketing: ['lang'] //Cookies in the marketing category.
 });
 
