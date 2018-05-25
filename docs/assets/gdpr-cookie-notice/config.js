43257ecb7eafaf63312f51dc847c3fce096198a4
DavidFirth <davidf@veryspeedy.net>
2018-05-25 10:13:07 +0100

    Tweak to cookie notice config.

--- docs/assets/gdpr-cookie-notice/config.js
+++ docs/assets/gdpr-cookie-notice/config.js
@@ -3,7 +3,7 @@ gdprCookieNotice({
   timeout: 500, //Time until the cookie bar appears
   expiration: 30, //This is the default value, in days
   domain: '.alt-3.uk', //If you run the same cookie notice on all subdomains, define the main domain starting with a .
-  implicit: false, //Deny cookies on scroll
+  implicit: true, //Allow cookies on scroll
   statement: '/cookies.html', //Link to your cookie statement page
 //  performance: ['mathjax'], //Cookies in the performance category.
   analytics: ['statcounter'], //Cookies in the analytics category.
